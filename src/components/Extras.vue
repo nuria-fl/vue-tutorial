ba2dc9de16c995869e49cf8bc7602dcbf9340757
Nuria <soriano.nuria@gmail.com>
2017-06-28 19:39:27 +0200

    add useful links

--- src/components/Extras.vue
+++ src/components/Extras.vue
@@ -9,6 +9,22 @@
       <li>Explora la <a href="https://www.themoviedb.org/documentation/api/discover" target="_blank">api de MovieDB</a> y crea otras secciones: películas más valoradas, estrenos, filtros por año, director…</li>
     </ul>
 
+    <h2>Recursos</h2>
+
+    <ul>
+      <li>
+        <a href="https://vuejs.org">Documentación oficial de Vue</a>
+      </li>
+      <li>
+        <a href="https://router.vuejs.org">Documentación oficial de Vue Router</a>
+      </li>
+      <li>
+        <a href="https://vuex.vuejs.org/en/">Documentación oficial de Vuex</a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/awesome-vue">Awesome Vue: Plugins, librerías, recursos, tutoriales…</a>
+      </li>
+    </ul>
   </div>
 </template>
 
